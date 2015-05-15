64b412510a9ed876621470b2eeaa92211caf744c
tsetoung <jtmoua@yahoo.com>
2015-05-15 09:23:08 -0400

    managing orders

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :profiles
+
   resources :listings do
-    resources :orders
+    resources :orders, only [:new, :create]
   end
 
   get 'pages/about'
   get 'pages/contact'
   get 'seller' => "listings#seller"
+  get 'sales' => "orders#sales"
+  get 'purchases' => "orders#purchases"
+
   root 'listings#index'
 end
