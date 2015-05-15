2bfec28df13e80fc67613e0ac0733cdcc2e6b80f
tsetoung <jtmoua@yahoo.com>
2015-05-15 05:05:58 -0400

    user validations

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,6 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, :default_url => "default.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+  validates :title, :description, :price, presence: true
+  validates :price, numericality: {greater_than: 0}
 end
